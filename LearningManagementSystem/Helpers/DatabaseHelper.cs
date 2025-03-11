c2a2e9f3789edef21005b01148f92517befe5d4a
Nithin Gannarpu <nithin.gsv@gmail.com>
2025-03-11 14:58:50 +0530

    helper methods and installed dapper

---
+++ LearningManagementSystem/Helpers/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace LearningManagementSystem.Helpers
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("PostgresConnection");
+        }
+
+        public IDbConnection GetConnection() => new NpgsqlConnection(_connectionString);
+    }
+}
---
+++ LearningManagementSystem/Repository/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using LearningManagementSystem.Helpers;
+using LearningManagementSystem.Models;
+
+public class EmployeeRepository
+{
+    private readonly DatabaseHelper _dbHelper;
+
+    public EmployeeRepository(DatabaseHelper dbHelper)
+    {
+        _dbHelper = dbHelper;
+    }
+
+    public IEnumerable<Employee> GetAllEmployees()
+    {
+        using (var db = _dbHelper.GetConnection())
+        {
+            return db.Query<Employee>("SELECT * FROM Employee");
+        }
+    }
+}
