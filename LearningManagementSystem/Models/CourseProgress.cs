4cf7a20774707af2cc2c18fcccc1b4430206cb22
balaji-winwire <balaji.nadakuditi@winwire.com>
2025-03-12 10:06:51 +0530

    Refactor Course and CourseProgress models
    - Updated Course model: replaced Topic with Title, added ResourceLink, Description, Skills, and Points. Modified TrainingSource and replaced Status with NewOrReUsed.
    - Commented out Progress and CourseContents in CourseProgress, along with their methods and the CourseContentItem class.
    - In RequestForm, replaced RequestedFor with RequestEmpIDs for better employee ID management.

--- LearningManagementSystem/Models/CourseProgress.cs
+++ LearningManagementSystem/Models/CourseProgress.cs
@@ -7,31 +7,31 @@ namespace LearningManagementSystem.Models
         public int ProgressID { get; set; }
         public int EmployeeID { get; set; }
         public int CourseID { get; set; }
-        public int Progress { get; set; }  // 0 to 100
-        public string Status { get; set; }  // "Not Started", "In Progress", "Completed"
+        //public int Progress { get; set; }  // 0 to 100
+        public string Status { get; set; }  // "default : Not Started", "In Progress", "Completed"
         public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public string MonthCompleted { get; set; }
-        public string CourseContents { get; set; }  // JSON string
+        //public string CourseContents { get; set; }  // JSON string
 
-        public List<CourseContentItem>? GetCourseContents()
-        {
-            return string.IsNullOrEmpty(CourseContents)
-                ? new List<CourseContentItem>()
-                : JsonSerializer.Deserialize<List<CourseContentItem>>(CourseContents);
-        }
+        //public List<CourseContentItem>? GetCourseContents()
+        //{
+        //    return string.IsNullOrEmpty(CourseContents)
+        //        ? new List<CourseContentItem>()
+        //        : JsonSerializer.Deserialize<List<CourseContentItem>>(CourseContents);
+        //}
 
-        public void SetCourseContents(List<CourseContentItem> contents)
-        {
-            CourseContents = JsonSerializer.Serialize(contents);
-        }
+        //public void SetCourseContents(List<CourseContentItem> contents)
+        //{
+        //    CourseContents = JsonSerializer.Serialize(contents);
+        //}
     }
 
-    public class CourseContentItem
-    {
-        public string Title { get; set; }
-        public string Link { get; set; }
-        public bool Completed { get; set; }
-    }
+    //public class CourseContentItem
+    //{
+    //    public string Title { get; set; }
+    //    public string Link { get; set; }
+    //    public bool Completed { get; set; }
+    //}
 }
