a0ad9c226a7ecc53588a32a6c8cf33e3207a07ed
Nithin Gannarpu <nithin.gsv@gmail.com>
2025-03-12 10:18:14 +0530

    request controller with ping

---
+++ LearningManagementSystem/Controllers/RequestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        [HttpGet]
+        public string ping()
+        {
+            return "pong";
+        }
+    }
+}
