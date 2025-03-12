a162a9ea1d9546a9961d2f72a6568972c321120d
balaji-winwire <balaji.nadakuditi@winwire.com>
2025-03-12 10:48:53 +0530

    Add CourseRequestFormRepository and interface implementation
    Implemented CourseRequestFormRepository for managing course request forms with methods for CRUD operations and status updates using Dapper. Added a new CourseRequestForm class implementing ICourseRequestForm with method stubs for future functionality.

---
+++ LearningManagementSystem/Repository/CourseRequestFormRepository.cs
@@ -0,0 +1,92 @@
+﻿using Dapper;
+using LearningManagementSystem.Helpers;
+using LearningManagementSystem.Models;
+
+namespace LearningManagementSystem.Repository
+{
+    public class CourseRequestFormRepository
+    {
+        private readonly DatabaseHelper _dbHelper;
+
+        public CourseRequestFormRepository(DatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        public IEnumerable<CourseRequestForm> GetAllCourseRequestForms()
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                return db.Query<CourseRequestForm>("SELECT * FROM CourseRequestForm");
+            }
+        }
+
+        public int CreateRequestForm(CourseRequestForm form)
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "INSERT INTO CourseRequestForm (EmployeeID, CourseID, RequestEmpIDs, RequestDate, Status, Comments, ImageLink) VALUES (@EmployeeID, @CourseID, @RequestEmpIDs, @RequestDate, @Status, @Comments, @ImageLink)";
+                int response = db.Execute(sql, form);
+                return response;
+            }
+        }
+
+        public CourseRequestForm UpdateRequestFormById(int id, CourseRequestForm form)
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "UPDATE CourseRequestForm SET EmployeeID = @EmployeeID, CourseID = @CourseID, RequestEmpIDs = @RequestEmpIDs, RequestDate = @RequestDate, Status = @Status, Comments = @Comments, ImageLink = @ImageLink WHERE RequestID = @RequestID";
+                db.Execute(sql, new { form.EmployeeID, form.CourseID, form.RequestEmpIDs, form.RequestDate, form.Status, form.Comments, form.ImageLink, RequestID = id });
+                return GetRequestById(id);
+            }
+        }
+
+        public void DeleteRequestFormById(int id)
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "DELETE FROM CourseRequestForm WHERE RequestID = @RequestID";
+                int response = db.Execute(sql, new { RequestID = id });
+            }
+        }
+
+        public void ApproveRequestForm(int id)
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "UPDATE CourseRequestForm SET Status = 'Approved' WHERE RequestID = @RequestID";
+                int response = db.Execute(sql, new { RequestID = id });
+            }
+        }
+
+        public void RejectRequestForm(int id)
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "UPDATE CourseRequestForm SET Status = 'Rejected' WHERE RequestID = @RequestID";
+                int response = db.Execute(sql, new { RequestID = id });
+            }
+        }
+
+        public List<CourseRequestForm> GetRequests()
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "SELECT * FROM CourseRequestForm";
+                List<CourseRequestForm> response =db.Query<CourseRequestForm>(sql).ToList();
+                return response;
+            }
+        }
+
+        public CourseRequestForm GetRequestById(int id)
+        {
+            using (var db = _dbHelper.GetConnection())
+            {
+                var sql = "SELECT * FROM CourseRequestForm WHERE RequestID = @RequestID";
+                CourseRequestForm response = db.QuerySingleOrDefault<CourseRequestForm>(sql, new { RequestID = id });
+                return response;
+            }
+        }
+
+    }
+}
---
+++ LearningManagementSystem/Services/CourseRequestForm.cs
@@ -0,0 +1,42 @@
+﻿using LearningManagementSystem.Services.IServices;
+
+namespace LearningManagementSystem.Services
+{
+    public class CourseRequestForm : ICourseRequestForm
+    {
+        public void ApproveRequestForm(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateRequestForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRequestFormById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseRequestForm GetRequestById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CourseRequestForm> GetRequests()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RejectRequestForm(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseRequestForm UpdateRequestFormById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
